664f5cd0f66bb364ffed5f8e595341eca8b53650
dtaniwaki <daisuketaniwaki@gmail.com>
2014-06-16 19:58:40 +0900

    Support avast

---
+++ lib/rack/secure_upload/scanner/avast.rb
@@ -0,0 +1,50 @@
+require 'rack/secure_upload/scanner/base'
+
+module Rack
+  module SecureUpload
+    module Scanner
+      class Avast < Base
+        def setup
+          raise SetupError, "#{options[:bin_path]} is not found." unless ::File.exists?(options[:bin_path])
+        end
+
+        def scan(path)
+          now_umask = ::File.umask(0)
+
+          lock do
+            output = command.run(path: path)
+            logger.info output
+          end
+
+          ::File.exist?(path)
+        ensure
+          ::File.umask(now_umask)
+        end
+
+        private
+
+        def lock
+          ::File.open(options[:lockfile_path], 'w', 0666) do |f|
+            f.flock(::File::LOCK_EX)
+            begin
+              yield
+            ensure
+              f.flock(::File::LOCK_UN)
+            end
+          end
+        end
+
+        def command
+          Cocaine::CommandLine.new(options[:bin_path], "-p 1 :path", :expected_outcodes => [0, 1])
+        end
+
+        def default_options
+          {
+            :bin_path => "/usr/bin/avast",
+            :lockfile_path => "/tmp/avast_lock"
+          }
+        end
+      end
+    end
+  end
+end
---
+++ spec/rack/secure_upload/scanner/avast_spec.rb
@@ -0,0 +1,51 @@
+require "spec_helper"
+
+describe Rack::SecureUpload::Scanner::Avast do
+  let(:path) { 'tmp/avast_spec_target' }
+  subject { Rack::SecureUpload::Scanner::Avast.new }
+
+  before do
+    ::File.open(path, 'w').close
+  end
+
+  describe "#setup" do
+    subject { Rack::SecureUpload::Scanner::Avast.new(bin_path: 'tmp/avast_binary') }
+    context "bin file exists" do
+      before do
+        ::File.open('tmp/avast_binary', 'w').close
+      end
+      it "does not raise an exception" do
+        expect{ subject.setup }.not_to raise_error
+      end
+    end
+    context "no bin file" do
+      it "raises an exception" do
+        expect{ subject.setup }.to raise_error(Rack::SecureUpload::SetupError)
+      end
+    end
+  end
+
+  describe "#scan" do
+    context "normal files" do
+      let(:command) { double run: lambda {} }
+      it "detects an insecure file" do
+        allow(subject).to receive(:command).and_return(command)
+        expect(subject.scan(path)).to eq(true)
+      end
+    end
+
+    context "insecure files" do
+      let(:command) do
+        d = double
+        allow(d).to receive(:run) do
+          ::FileUtils.rm path
+        end
+        d
+      end
+      it "does not detect normal file" do
+        allow(subject).to receive(:command).and_return(command)
+        expect(subject.scan(path)).to eq(false)
+      end
+    end
+  end
+end
